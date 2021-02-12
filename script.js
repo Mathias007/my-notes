379250ed37c54c0b874687347f88df624b8a121e
Mathias007 <mathias.najlepszy@gmail.com>
2021-02-12 19:03:09 +0100

    handle adding new note

--- script.js
+++ script.js
@@ -25,5 +25,46 @@ const closePanel = () => {
     category.selectedIndex = 0;
 };
 
+const addNote = () => {
+    console.log(category.options[category.selectedIndex].value);
+    if (
+        textarea.value !== "" &&
+        category.options[category.selectedIndex].value !== "0"
+    ) {
+        createNote();
+        error.style.visibility = "hidden";
+    } else {
+        error.style.visibility = "visible";
+    }
+};
+
+const createNote = () => {
+    const newNote = document.createElement("div");
+    newNote.classList.add("note");
+    newNote.setAttribute("id", cardID);
+    newNote.innerHTML = `
+        <div class="note-header">
+            <h3 class="note-title">${selectedValue}</h3>
+                <button class="delete-note">
+                    <i class="fas fa-times icon"></i>
+                </button>
+            </div>
+        <div class="note-body">
+            ${textarea.value}
+        </div>
+    `;
+
+    noteArea.appendChild(newNote);
+    cardID++;
+    textarea.value = "";
+    category.selectedIndex = 0;
+    notePanel.style.display = "none";
+};
+
+const selectValue = () => {
+    selectedValue = category.options[category.selectedIndex].text;
+};
+
 addBtn.addEventListener("click", openPanel);
 cancelBtn.addEventListener("click", closePanel);
+saveBtn.addEventListener("click", addNote);
